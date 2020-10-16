26012fb20fbce89a91e1821ff674ee6763d8f030
Tomer Blecher <tomer.blecher.brolix@gmail.com>
2020-10-16 15:54:34 +0300

    Generic setting widget (#22)
    * Generic setting widget
    
    * Review Fix

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:list_tile_more_customizable/list_tile_more_customizable.dart';
+import 'package:ombiapp/pages/settings/setting_category_container.dart';
+import 'package:ombiapp/pages/settings/setting_container.dart';
 import 'package:ombiapp/services/settings_service.dart';
 import 'package:ombiapp/utils/input_formatter.dart';
-import 'package:ombiapp/utils/utilsImpl.dart';
 import 'package:ombiapp/widgets/digit_form_field.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -23,6 +22,12 @@ class _SettingsPageState extends State<SettingsPage> {
   }
 
   @override
+  void dispose() {
+    _searchDelayController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
         child: SingleChildScrollView(
@@ -33,42 +38,19 @@ class _SettingsPageState extends State<SettingsPage> {
               title: Text(
             "Settings Page",
           )),
-          Container(
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Padding(
-                      padding: EdgeInsets.fromLTRB(5, 15, 5, 8),
-                      child: Text(
-                        "Search System",
-                        style: TextStyle(fontWeight: FontWeight.w400),
-                      )),
-                  ListTileMoreCustomizable(
-                      horizontalTitleGap: 0.5,
-                      leading: Icon(
-                        Icons.youtube_searched_for,
-                      ),
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text('Search Delay'),
-                          Text(
-                            'Delay duration should be between 1-5 seconds',
-                            style:
-                                TextStyle(fontSize: 12, color: Colors.black26),
-                          ),
-                        ],
-                      ),
-                      trailing: Container(
-                          width: UtilsImpl.getScreenWidth(context) * 0.15,
-                          child: DigitInputField(
-                            callback: _settingsService.updateLearnImgCount,
-                            controller: _searchDelayController,
-                            formatter: DigitTextInputFormatter(1, 5),
-                          ))),
-                ],
-              )),
+          SettingCategoryContainer(
+              title: 'Search System',
+              settings: <SettingContainer>[
+                SettingContainer(
+                  title: Text('Search Delay'),
+                  icon: Icon(Icons.youtube_searched_for),
+                  input: DigitInputField(
+                    callback: _settingsService.updateSearchDelay,
+                    controller: _searchDelayController,
+                    formatter: DigitTextInputFormatter(1, 5),
+                  ),
+                ),
+              ]),
           SizedBox(
             height: 5,
           ),
